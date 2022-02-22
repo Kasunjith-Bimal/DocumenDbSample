8faf6b211b46a974edeee161b59cf06f0776de72
Kasunjith-Bimal <kasunysoft@gmail.com>
2022-02-22 23:58:35 +0530

    document Db

---
+++ CosomoDb/Controllers/ItemController.cs
@@ -0,0 +1,73 @@
+﻿using CosomoDb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosomoDb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IDocumentClient _doumentClient;
+        readonly string _databaseId;
+        readonly string _collectionId;
+        public IConfiguration _configuration;
+
+        public ItemController(IDocumentClient doumentClient, IConfiguration configuration)
+        {
+            _doumentClient = doumentClient;
+            _configuration = configuration;
+            _databaseId = _configuration["DatabaseId"];
+            _collectionId = "Items";
+            BuildCollection().Wait();
+        }
+        private async Task BuildCollection()
+        {
+            await _doumentClient.CreateDatabaseIfNotExistsAsync(new Database { Id = _databaseId });
+            await _doumentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(_databaseId), new DocumentCollection { Id = _collectionId });  
+        }
+
+        [HttpGet]
+        public IQueryable<Item> Get()
+        {
+            return _doumentClient.CreateDocumentQuery<Item>(UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionId),
+                new FeedOptions { MaxItemCount = 20 });
+        }
+
+        [HttpGet("{id}")]
+        public IQueryable<Item> Get(string id)
+        {
+            return _doumentClient.CreateDocumentQuery<Item>(UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionId),
+                new FeedOptions { MaxItemCount = 1 }).Where((i) => i.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Item item)
+        {
+            var response = await _doumentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(_databaseId, _collectionId), item);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] Item item)
+        {
+            await _doumentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(_databaseId, _collectionId, id),
+                item);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _doumentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(_databaseId, _collectionId, id));
+            return Ok();
+        }
+    }
+}
---
+++ CosomoDb/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosomoDb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(); 
+            services.AddSingleton<IDocumentClient>(x => new DocumentClient(new Uri(Configuration["CosmoDB:URL"]), Configuration["CosmoDB:PrimaryKey"]));
+            var info = new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = "Swagger Demo API",
+                Description = "Swagger Demo API Description",
+                TermsOfService = new Uri("http://www.example.com"),
+               
+            };
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", info);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                "Swagger Demo API v1");
+            });
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+          
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
